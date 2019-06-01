4bf7d2bfb728abfd8053452d148c2382ab235cad
LemurPwned <jakub.mojsiejuk@gmail.com>
2019-06-01 09:41:49 +0200

    chainer

--- ball_detect.py
+++ ball_detect.py
@@ -6,6 +6,12 @@ import imutils
 from scipy.spatial import distance as dist
 
 
+picked_ball_position = None
+
+
+def distance(x, y): return np.sqrt((x[0]-y[0])**2 + (x[1]-y[1])**2)
+
+
 def is_ROI(center, pos, tolerance=100):
     """
     is tracked in roi?
@@ -17,13 +23,33 @@ def is_ROI(center, pos, tolerance=100):
     return False
 
 
+def set_ball_position(event, x, y, flags, param):
+    global picked_ball_position
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        picked_ball_position = (x, y)
+        print(picked_ball_position)
+        cv2.circle(image, picked_ball_position, 2, (255, 255, 0), 2)
+
+
+def find_min_distance_to_last_ball_position(candidate_points, last_ball_position):
+    min_distance = 9999
+    min_distance_index = 0
+    for i, candidate_pos in enumerate(candidate_points):
+        dist = np.sqrt((candidate_pos[0] - last_ball_position[0])**2 +
+                       (candidate_pos[1] - last_ball_position[1]))
+        if dist < min_distance:
+            min_distance = dist
+            min_distance_index = i
+    return candidate_points[min_distance_index]
+
+
 def find_min_distance_index(persistence_list, roi):
     """
     find index of min euclidian distance of roi objects
     """
     min_dist_index = 0
     min_dist = 999999
-    x, y, w, h = roi
+    x, y, _, _ = roi
     for i, obj in enumerate(persistence_list):
         if obj.distance(*(x, y)) <= min_dist:
             min_dist = obj.distance(*(x, y))
@@ -33,7 +59,7 @@ def find_min_distance_index(persistence_list, roi):
 
 def find_max_persistence(persistence_list):
     """
-    locate max persistence object 
+    locate max persistence object
     """
     max_per = 0
     for obj in persistence_list:
@@ -91,15 +117,41 @@ kernel = np.ones((13, 13), np.uint8)
 persistent = []
 ball_positions, ball_speeds = [], [0]
 acc_pos = (0, 0)
+
+while True:
+    idx += 1
+    rate, image = cap.read()
+    if idx < 120:
+        continue
+    else:
+        break
+
+image = imutils.resize(image, width=700)
+while True:
+    cv2.imshow('Match Detection', image)
+    cv2.setMouseCallback('Match Detection', set_ball_position)
+    if picked_ball_position is None:
+        k = cv2.waitKey(20) & 0xFF
+        if k == ord('q'):
+            break
+        elif k == ord('a'):
+            print(picked_ball_position)
+            cv2.circle(image, picked_ball_position, 2, (255, 255, 0), 2)
+    else:
+        break
+
 while True:
     idx += 1
     rate, image = cap.read()
     image = imutils.resize(image, width=700)
     (ih, iw) = image.shape[:2]
+    cv2.imshow('Match Detection', image)
+    # cv2.setMouseCallback('Match Detection', set_ball_position)
+    new_ball_pos = picked_ball_position
     center = (int(iw/2), int(ih/2))
 
-    if idx <= 100:
-        continue
+    # if idx <= 100:
+    #     continue
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     sensitivity = 85
     lower_white = np.array([0, 0, 255-sensitivity])
@@ -113,6 +165,7 @@ while True:
 
     contours, hierarchy = cv2.findContours(
         res, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    candidates = []
     for contour in contours:
         x, y, w, h = cv2.boundingRect(contour)
         roi = (x, y, w, h)
@@ -125,8 +178,9 @@ while True:
                     continue
                 area = np.pi*(radius**2)
                 guessed_area = cv2.contourArea(contour)
-                if guessed_area <= 8 and guessed_area >= 3:
+                if guessed_area <= 7 and guessed_area >= 3:
                     center_x = (int(x), int(y))
+                    candidates.append(center_x)
                     radius = int(radius)
                     min_distance_index, min_distance = find_min_distance_index(
                         persistent, roi)
@@ -137,32 +191,41 @@ while True:
                     # we have caught the ball candidate, we track it now
                     cv2.circle(image, center_x, radius, (0, 255, 0), 2)
 
-    to_delete = []
-    ball = False
-    max_per = find_max_persistence(persistent)
-    for i in range(len(persistent)):
-        persistent[i].frames += 1
-        if persistent[i].persistence_count > 1:
-            if (persistent[i].persistence_count == max_per) and not ball:
-                x, y, w, h = persistent[i].roi()
-                cv2.rectangle(image, (x, y), (x+w, y+h),
-                              (0, 0, 255), 3)
-                ball = True
-                ball_positions.append([x, y])
-                if len(ball_positions) != 1:
-                    p_ball = ball_positions[-2]
-                    ball_speeds.append((p_ball[0]-x, p_ball[1]-y))
-                    acc_pos = (acc_pos[0]+p_ball[0]-x, acc_pos[1]+p_ball[1]-y)
-        if ((persistent[i].persistence_count/persistent[i].frames) < 0.4):
-            if persistent[i].frames > 3:
-                to_delete.append(i)
-    for index in sorted(to_delete, reverse=True):
-        del persistent[index]
+    print(candidates)
+    if candidates != []:
+        new_ball_pos = find_min_distance_to_last_ball_position(
+            candidates, new_ball_pos)
+        if ball_positions:
+            ball_speeds.append(distance(new_ball_pos, ball_positions[-1]))
+        ball_positions.append(new_ball_pos)
+        cv2.circle(image, new_ball_pos, 2, (100, 0, 120), 2)
+    # to_delete = []
+    # ball = False
+    # max_per = find_max_persistence(persistent)
+    # for i in range(len(persistent)):
+    #     persistent[i].frames += 1
+    #     if persistent[i].persistence_count > 1:
+    #         if (persistent[i].persistence_count == max_per) and not ball:
+    #             x, y, w, h = persistent[i].roi()
+    #             cv2.rectangle(image, (x, y), (x+w, y+h),
+    #                           (0, 0, 255), 3)
+    #             ball = True
+    #             ball_positions.append([x, y])
+    #             if len(ball_positions) != 1:
+    #                 p_ball = ball_positions[-2]
+    #                 ball_speeds.append((p_ball[0]-x, p_ball[1]-y))
+    #                 acc_pos = (acc_pos[0]+p_ball[0]-x, acc_pos[1]+p_ball[1]-y)
+    #     if ((persistent[i].persistence_count/persistent[i].frames) < 0.4):
+    #         if persistent[i].frames > 1:
+    #             to_delete.append(i)
+
+    # for index in sorted(to_delete, reverse=True):
+    #     del persistent[index]
     if ball_positions != [] and ball_speeds != []:
         field_half = 'left' if acc_pos[0] < 0 else 'right'
         info = {
             "ball pos": ball_positions[-1],
-            "ball speed": ball_speeds[-1],
+            "ball speed": np.around(ball_speeds[-1], decimals=2),
             "half": field_half
         }
         # loop over the info tuples and draw them on our frame
